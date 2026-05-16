21e015019a094b6b03df7f6040ee0537d5bfb48d
Jean Etzer Charles <jeanetzercharles@gmail.com>
2026-05-16 10:58:15 +0200

    first portfolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div>
+      <h1>Jean Etzer Charles</h1>
+      <h2>Data Analyst | Econometrics Student</h2>
+
+      <p>
+        Welcome to my professional portfolio website.
+      </p>
+
+      <h3>Skills</h3>
+
+      <ul>
+        <li>Python</li>
+        <li>R Studio</li>
+        <li>SQL</li>
+        <li>Power BI</li>
+        <li>Econometrics</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
